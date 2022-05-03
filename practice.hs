bea2ca6392f70c216a880582dab863bb57780f2f
bryan <bryanfeliciano892@gmail.com>
2022-05-03 17:23:28 -0500

    implementation of functors

--- practice.hs
+++ practice.hs
@@ -1,14 +1,30 @@
-import Data.Maybe
-import Data.Semigroup
-import Data.Functor
--- Write the function reverseMaybe :: Maybe String -> Maybe String that
--- reverses a Maybe String and returns it as a Maybe String
+data RobotPart = RobotPart {
+                            name :: String,
+                            description :: String,
+                            cost :: Double,
+                            count :: Int
+                           } deriving Show
 
-reverseMaybe :: Maybe String -> Maybe String
-reverseMaybe Nothing = Nothing
-reverseMaybe (Just string) = Just (reverse string)
+leftArm :: RobotPart
+leftArm = RobotPart
+                    { name = "left arm"
+                    , description = "left arm for face punching!"
+                    , cost = 1000.00
+                    , count = 3
+                    }
 
--- Making Maybe an instance of Functor
-instance Functor Maybe where
-    fmap func (Just n) = Just (func n)
-    fmap func Nothing = Nothing 
+rightArm :: RobotPart
+rightArm = RobotPart
+                    { name = "right arm"
+                    , description = "right arm for kind hand gestures"
+                    , cost = 1025.00
+                    , count = 5
+                    }
+
+robotHead :: RobotPart
+robotHead = RobotPart
+                    { name = "robot head"
+                    , description = "this head looks mad"
+                    , cost = 5092.25
+                    , count = 2
+                    }
