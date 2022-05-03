fb9e0106e70772ee1e8ca70fba10aa26121a94a1
bryan <bryanfeliciano892@gmail.com>
2022-05-03 17:48:21 -0500

    render html,and partsDb added

--- practice.hs
+++ practice.hs
@@ -1,3 +1,7 @@
+import qualified Data.Map as Map
+
+type Html = String
+
 data RobotPart = RobotPart {
                             name :: String,
                             description :: String,
@@ -28,3 +32,24 @@ robotHead = RobotPart
                     , cost = 5092.25
                     , count = 2
                     }
+
+renderHtml :: RobotPart -> Html
+renderHtml part = mconcat ["<h2>",partName, "</h2>"
+                          ,"<p><h3>desc</h3>",partDesc
+                          ,"</p><p><h3>cost</h3>"
+                          ,partCost
+                          ,"</p><p><h3>count</h3>"
+                          ,partCount,"</p>"]
+    where 
+        partName = name part
+        partDesc = description part
+        partCost = show (cost part)
+        partCount = show (count part)
+
+partsDB :: Map.Map Int RobotPart
+partsDB = Map.fromList keyVals
+    where 
+        keys = [1,2,3]
+        vals = [leftArm,rightArm,robotHead]
+        keyVals = zip keys vals
+
