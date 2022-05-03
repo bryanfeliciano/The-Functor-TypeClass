fe11fc574dba86b49d3ba1b4ebe392906835b32c
bryan <bryanfeliciano892@gmail.com>
2022-05-03 18:34:39 -0500

    final commit

--- practice.hs
+++ practice.hs
@@ -67,3 +67,56 @@ allPartsHtml = renderHtml <$> allParts
 
 htmlPartsDB :: Map.Map Int Html
 htmlPartsDB = renderHtml <$> partsDB
+
+-- transformation example --
+
+leftArmIO :: IO RobotPart
+leftArmIO = return leftArm
+
+-- Q27.1 When we introduced parameterized types in lesson 15, you used a minimal
+-- type Box as the example:
+
+-- data Box a = Box a deriving Show
+
+-- Implement the Functor type class for Box. Then implement morePresents, which changes a
+-- box from type Box a to one of type Box [a], which has n copies of the original value in the
+-- box in a list. Make sure to use fmap to implement this.
+
+data Box a = Box a deriving Show
+
+instance Functor Box  where
+    fmap func (Box val) = Box (func val)
+
+
+-- QC27.2 Now suppose you have a simple box like this:
+-- myBox :: Box Int
+-- myBox = Box 1
+-- Use fmap to put the value in your Box in another Box. Then define a function unwrap that
+-- takes a value out of a box, and use fmap on that function to get your original box. Here’s
+-- how your code should work in GHCi:
+-- GHCi> wrapped = fmap ? myBox
+-- GHCi> wrapped
+-- Box (Box 1)
+-- GHCi> fmap unwrap wrapped
+-- Box 1
+
+myBox :: Box Int
+myBox = Box 1
+
+unwrap :: Box a -> a
+unwrap (Box val) = val 
+
+-- Q27.3 Write a command-line interface for partsDB that lets the user look up the cost of
+-- an item, given an ID. Use the Maybe type to handle the case of the user entering missing
+-- input.
+
+printCost :: Maybe Double -> IO()
+printCost nothing = putStrLn "Item not Found"
+printCost (Just cost) = print cost
+
+main :: IO()
+main = do
+    putStrLn "enter a part number"
+    PartNo <- getLine
+    let part = Map.lookUp (read partNo) partsDB
+    printCost (cost <$> part)
